661117036de517dc3f44acf5fb6c9c133ff5dd78
ArthurLorenzo230 <arthurlorenzo@colegiopaulodetarso.com.br>
2026-08-11 10:54:04 +0000

    live

--- app.py
+++ app.py
@@ -10,3 +10,8 @@ def index():
 def boletim():
     return render_template('boletim.html', title="Boletim")
 
+@app.route("/sobre-mim")
+def sobre_mim():
+    foto_url = "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?auto=format&fit=crop&w=400&q=80"
+    return render_template('sobre_mim.html', title="Sobre mim", foto_url=foto_url)
+
